a6d3c75e90f73c4a0ca154ce67dbeb9f6c6ccb81
Greg Doud <gpdoud@gmail.com>
2020-06-18 13:12:53 -0400

    Add project files.

---
+++ SwitchStatement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SwitchStatement {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Enter state code : ");
+            var statecode = Console.ReadLine();
+            var stcd = statecode.ToLower();
+
+            switch(stcd) {
+                case "oh": Console.WriteLine($"State is Ohio"); break;
+                case "mt": Console.WriteLine($"State is Montana"); break;
+                case "az": Console.WriteLine($"State is Arizona"); break;
+                default: Console.WriteLine($"State code is not found"); break;
+
+            }
+
+        }
+    }
+}
